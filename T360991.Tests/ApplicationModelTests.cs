595e474e9e12313d3e3adedfa54c72e88964f0d8
Manuel Grundner <m.grundner@delegate.at>
2016-04-12 09:17:15 +0200

    Added Test assembly

---
+++ T360991.Tests/ApplicationModelTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Win.SystemModule;
+using Shouldly;
+using T360991.Module;
+using T360991.Module.Model;
+using T360991.Module.Win;
+using Xunit;
+
+namespace T360991.Tests
+{
+    public class ApplicationModelTests : IDisposable
+    {
+        TestApplication Application { get; set; }
+
+        public ApplicationModelTests()
+        {
+            Application = new TestApplication();
+            Application.Modules.Add(new SystemModule());
+            Application.Modules.Add(new SystemWindowsFormsModule());
+            Application.Modules.Add(new T360991Module());
+            Application.Modules.Add(new T360991WindowsFormsModule());
+            Application.Setup();
+        }
+
+        [Fact]
+        public void ApplicationModelIsOptions()
+        {
+            var result = Application.Model is IModelApplicationCommandlineOptions;
+            result.ShouldBe(true);
+        }
+
+        [Fact]
+        public void DefaultValueForSingleInstanceIsTrue()
+        {
+            var model = Application.Model as IModelApplicationCommandlineOptions;
+            model.CommandlineOptions.SingeInstance.ShouldBe(true);
+        }
+
+        public void Dispose()
+        {
+            Application?.Dispose();
+            Application = null;
+        }
+    }
+}
---
+++ T360991.Tests/TestApplication.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Layout;
+
+namespace T360991.Tests
+{
+    public class TestApplication : XafApplication
+    {
+        protected override LayoutManager CreateLayoutManagerCore(bool simple)
+        {
+            return null;
+        }
+
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args)
+        {
+            args.ObjectSpaceProvider = new NonPersistentObjectSpaceProvider();
+        }
+    }
+}
